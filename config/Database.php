3134144d13909e981d7e0d35998dd6760d52182a
Ahmad Khaliq <106729589+AhmadKhaliqIT@users.noreply.github.com>
2022-07-27 12:06:50 +0430

    Fix Some bugs

--- config/Database.php
+++ config/Database.php
@@ -2,8 +2,8 @@
 
 return [
    'servername' => 'localhost',
-   'username'   => 'fw_test',
-   'password'   => 'EgF9PD5CD',
-   'dbname'     => 'fw_test',
+   'username'   => '',
+   'password'   => '',
+   'dbname'     => '',
    'ConnectAutomatically'  => true,
 ];
