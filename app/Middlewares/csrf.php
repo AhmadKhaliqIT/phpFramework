e068050890f3a59d2916dcb32391637619ca6d43
Ahmad Khaliq <ahmad.khaliq@yahoo.com>
2022-08-02 16:07:49 +0430

    Creating CSRF system

---
+++ app/Middlewares/csrf.php
@@ -0,0 +1,39 @@
+<?php
+/* بسم الله الرحمن الرحیم */
+/**
+ * phpFramework
+ *
+ * @author     Ahmad Khaliq
+ * @author     Mojtaba Zadegi
+ * @copyright  2022 Ahmad Khaliq
+ * @license    https://github.com/AhmadKhaliqIT/phpFramework/blob/main/LICENSE
+ * @link       https://github.com/AhmadKhaliqIT/phpFramework/
+ */
+
+
+
+namespace App\Middlewares;
+
+use Closure;
+use Core\Browser\Session;
+use Core\Http\Request;
+use Core\Auth\Auth as AuthBass;
+use Core\Auth\Guard;
+
+class Auth
+{
+    private string $Current_Method;
+    private array $except_methods=[];
+
+
+    public function handle(Request $request)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST')
+            if(Core()->Session()->token() !== $request->_token)
+                abort(403);
+    }
+
+
+
+
+}
